48453286d9ee73fd08ac58895cceb6ca3012cabc
TahaYil <taha.yldrms1@gmail.com>
2025-05-10 16:18:15 +0300

    görsele tresh hold uygulayıp okuma yapma

---
+++ main.py
@@ -0,0 +1,38 @@
+from sympy import false
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image,ImageOps,ImageEnhance
+import torch
+
+# Model ve işlemciyi yükle
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten',use_fast=False)
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
+
+#TÜM GÖRSELDE İŞE YARAMIYOR SATIR SATIR
+image = Image.open("fsatir.png")
+
+# GRİYE ÇEVİRME
+gray_image = image.convert("L")
+
+# TRESHOLD
+threshold_image = gray_image.point(lambda x: 0 if x < 140 else 255, '1')
+
+# RGB ÇEVİRME
+threshold_rgb = threshold_image.convert("RGB")
+
+threshold_rgb.show()
+#image=image.convert("RGB")
+
+# Modelle işlem yaparken kullanmak için:
+pixel_values = processor(images=threshold_rgb, return_tensors="pt").pixel_values
+
+# Tahmin
+generated_ids = model.generate(pixel_values)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print("\n🖋️ Tanınan Metin:\n")
+print(generated_text)
+
+
+
+
+
